bf5c4d6b55544f503b65ee2dff43046cdcb2ba41
jpromanonet <programming@jpromano.net>
2022-12-26 16:04:06 -0300

    Clock in CPP

---
+++ 01_clock/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    int h, m, s, a, err;
+    err = a = 0;
+    while(err == 0){
+        cout << "Enter hours: " << endl;
+        cin >> h;
+        cout << "Enter minutes: " << endl;
+        cin >> m;
+        cout << "Enter seconds: " << endl;
+        cin >> s;
+    }
+}
